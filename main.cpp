a16a6843011ed8e10b8b0fea769c518c31950977
Anirudh Singh <anirudh.sai8296@gmail.com>
2024-02-25 11:19:03 +0100

    Version 1 updated in main branch

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 int main(int argc, char* argv[])
 {
-    std::cout << "Version 3" << std::endl;
+    std::cout << "Version 1 updated in main branch" << std::endl;
     
     return EXIT_SUCCESS;
 }
