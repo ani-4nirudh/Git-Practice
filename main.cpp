1ed225dffb09ccd7607c00918b700a80a3e39ad6
Anirudh Singh <anirudh.sai8296@gmail.com>
2025-09-30 21:58:22 +0200

    TLF: Added a new development branch

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 int main(int argc, char* argv[])
 {
-    std::cout << "Version 14: Using tags" << std::endl;
+    std::cout << "Version 15: Created a development branch" << std::endl;
     
     return EXIT_SUCCESS;
 }
